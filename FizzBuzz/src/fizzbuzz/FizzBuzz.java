d99244039818bbe99e32104b73010a806e4c6848
J Feustel <jfeustel@gmail.com>
2016-08-17 13:30:21 -0500

    Comments
    Updated comments and javadoc

--- FizzBuzz/src/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -3,16 +3,17 @@ package fizzbuzz;
 /**
  *
  * @author J Feustel
+ * 8/26/2016
  * FizzBuzz solution with Java
+ * 
  */
 public class FizzBuzz {
 
     /**
      * @param args the command line arguments
-     * @param 
      */
     public static void main(String[] args) {
-        for(int i = 1; i <= 100; i++){
+        for(int i = 1; i <= 100; i++){ //counts from 1 to 100, allowing program to check for divisibility of 3, 5, both, or neither.
             if(i % 3 == 0 && i % 5 == 0){
                 System.out.println("FizzBuzz");                
             }
